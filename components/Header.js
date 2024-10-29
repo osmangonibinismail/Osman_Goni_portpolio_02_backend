47be5441dd0500f81e4d433c7c388da6296fcdce
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-10-30 02:25:44 +0600

    header section

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,25 @@
+import Link from "next/link";
+import { IoHome } from "react-icons/io5";
+import { BsPostcard } from "react-icons/bs";
 
 
 export default function Header() {
-   
-    return <>
 
+    return <>
+        <aside className="asideleft active">
+            <ul>
+                <Link href='/'>
+                    <li className="navactive">
+                        <IoHome />
+                    </li>
+                </Link>
+                <li className="navactive flex-col flex-left">
+                    <div className="flex gap-1">
+                        <sPostcard />
+                        <span>Blogs</span>
+                    </div>
+                </li>
+            </ul>
+        </aside>
     </>
 }
